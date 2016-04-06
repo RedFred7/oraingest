fe403325e5f91afb58df1e52f6443809b1847e56
Fred Heath <fred_h@bootstrap.me.uk>
2016-04-06 15:21:25 +0100

    added 'reviewer cannot claim an assigned item' test

--- test/integration/review_dashboard_test.rb
+++ test/integration/review_dashboard_test.rb
@@ -13,12 +13,12 @@ class ReviewDashboardTest < CapybaraTest
     assert_equal page.has_text?("#{NO_OF_TEST_DATA_ITEMS} Items found"), true
   end
 
- test 'reviewer can see all unclaimed items' do
+  test 'reviewer can see all unclaimed items' do
     visit '/dash'
     click_link("Show all items")
     click_link("All unclaimed items")
     assert_equal page.has_text?("#{NO_OF_TEST_DATA_ITEMS-1} Items found"), true
-    end  
+  end
 
 
   test "reviewer can claim a draft item" do
@@ -34,14 +34,12 @@ class ReviewDashboardTest < CapybaraTest
 
 
   test "reviewer cannot claim an assigned item" do
-    # visit '/dash'
-    # title_to_claim = @test_data.first["desc_metadata__title_tesim"].first
-    # fill_in('dash_search', :with => title_to_claim.gsub(/\s/, '+'))
-    # click_button('dash_submit')
-    # assert_equal page.has_text?("1 Items found"), true
-    # click_link('claim_item_btn')
-    # assert_equal page.has_selector?('span.tag.tag-claimed'), true
-    # assert_equal page.has_selector?('a#unclaim_item_btn'), true
+    visit '/dash'
+    title_to_claim = get_test_data_with_status(Sufia.config.assigned_status).first.title
+    fill_in('dash_search', :with => title_to_claim.gsub(/\s/, '+'))
+    click_button('dash_submit')
+    assert_equal page.has_text?("1 Items found"), true
+    refute_equal page.has_selector?('claim_item_btn'), true
   end
 
 
